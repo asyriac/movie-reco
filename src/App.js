cd6f0611dca264560167a38a2e2a8bcef7b838a7
arun_syriac <arundegr8@gmail.com>
2020-12-18 21:32:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { dict } from "./data";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  function onClickHandler(genre) {
+    console.log(genre);
+    const movieList = dict[genre];
+    console.log(movieList);
+    setMovies(movieList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Recos</h1>
+      <ul className="navbar">
+        <li onClick={() => onClickHandler("action")}>Action</li>
+        <li onClick={() => onClickHandler("adventure")}>Adventure</li>
+        <li onClick={() => onClickHandler("comedy")}>Comedy</li>
+        <li onClick={() => onClickHandler("thriller")}>Thriller</li>
+        <li onClick={() => onClickHandler("horror")}>Horror</li>
+      </ul>
+
+      <ul className="movieList">
+        {movies.map(function (movie, index) {
+          return (
+            <li key={index} className="movie-item">
+              <img src={movie.image} />
+              <div className="movie-details">
+                <h1 className="movie-title">{movie.name}</h1>
+                <p className="movie-summary">{movie.summary}</p>
+                <p className="movie-rating">Rating: {movie.rating}%</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,104 @@
+export const dict = {
+   action : [
+      {
+         name : "Black Panther",
+         summary: "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
+         rating: 96,
+         image: "https://resizing.flixster.com/P3XrfzcI9ycfMEdR6iqwPPv0rvU=/206x305/v2/https://resizing.flixster.com/dH6cVqiBByzFjrjLDr9NW9dSdQY=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzMxOGI1YzBhLWMyMjEtNGUxMS1iM2Q0LWQ4OGMyYzQyZjQyYS53ZWJw",
+      },
+      {
+         name : "Avengers: Endgame",
+         summary : "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+         rating: 94,
+         image: "https://resizing.flixster.com/gAReGGuBwusDYD3MQX3gl7dfsCE=/fit-in/200x296.2962962962963/v1.bTsxMzAxOTkzMjtqOzE4NjQ1OzEyMDA7MTY4ODsyNTAw",
+      },
+      {
+         name : "Mission: Impossible - Fallout",
+         summary : "Ethan Hunt and his IMF team, along with some familiar allies, race against time after a mission gone wrong.",
+         rating: 92,
+         image: "https://resizing.flixster.com/bRNSA34xdPlV0N-2wTQuQjDW_U8=/206x305/v2/https://resizing.flixster.com/1dOEFq-TDKU4MtMZbjO3gwTyK1M=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzhiMWVjNTExLTEwNjMtNDk2NC1iY2JhLTY1YWIyYTk1Njk1ZC53ZWJw",
+      },
+   ],
+   adventure : [
+      {
+         name : "Elf",
+         summary: "After discovering he is a human, a man raised as an elf at the North Pole decides to travel to New York City to locate his real father.",
+         rating: 92,
+         image: "https://resizing.flixster.com/4KgMikhwITyd71pnY956LK34JLc=/206x305/v2/https://flxt.tmsimg.com/assets/p32828_p_v10_am.jpg",
+      },
+      {
+         name : "Harry Potter and the Sorcerer's Stone",
+         summary: "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.",
+         rating: 88,
+         image: "https://resizing.flixster.com/Q5W7m_i_f24Q_a4zLeRxNvx1WAs=/206x305/v2/https://flxt.tmsimg.com/assets/p28630_p_v8_at.jpg",
+      },
+      {
+         name : "Gladiator",
+         summary: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+         rating: 77,
+         image: "https://resizing.flixster.com/vkK8qxwqVqm_u6Y7vmmSM95rRkY=/206x305/v2/https://flxt.tmsimg.com/assets/p24674_p_v10_ag.jpg",
+      },
+   ],
+   comedy : [
+      {
+         name : "Toy Story 4",
+         summary : "When a new toy called Forky joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy.",
+         rating: 97,
+         image: "https://resizing.flixster.com/mkKEWnuNTVfLyRdiLCh-ByZU4Lc=/206x305/v2/https://resizing.flixster.com/kBmorcZ1u256R5vlVLes4uQDWTA=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc5ZTJhYjFiLTkwNzMtNDdjZS1hY2Q2LTNlN2NjNTZiYWFmNC53ZWJw",
+      },
+      {
+         name : "Coco",
+         summary : "Aspiring musician Miguel, confronted with his family's ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.",
+         rating: 96,
+         image: "https://resizing.flixster.com/agnmjtlNAv9G8iw5b70dRe2gCcM=/206x305/v2/https://resizing.flixster.com/xx-TYHWok94OgVCI5VEqrQGBZF0=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzk3YzAyMWIyLTc0MmMtNDI4MS05ZTMyLWMwNGQwYjAyNDY0OC53ZWJw",
+      },
+      {
+         name : "Lady Bird",
+         summary : "In 2002, an artistically inclined seventeen-year-old girl comes of age in Sacramento, California.",
+         rating: 93,
+         image: "https://resizing.flixster.com/CmD0J-DN-uBKKJeMbhz6po38fso=/206x305/v2/https://resizing.flixster.com/1f9bH_99aSYNgLROtrUDzyuN8ek=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzI4MjU0NjdiLTMwYTQtNDVmNy1hYjdjLWYwNTk5NTc0MGQ3MC53ZWJw",
+      },
+   ],
+   thriller : [
+      {
+         name : "Get Out",
+         summary : "A young African-American visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point.",
+         rating: 98,
+         image: "https://resizing.flixster.com/WQvxDGdZxKzj53GK3fVbkC-2eC8=/206x305/v2/https://resizing.flixster.com/Q2uwvQLSNT5ObCtXKALBjceWDbw=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzNiZGVhNTVmLWQzOGUtNGMwZC1hYmZjLTIxZWE5NThkMTdjNS53ZWJw",
+      },
+      {
+         name : "Argo",
+         summary : "Acting under the cover of a Hollywood producer scouting a location for a science fiction film, a CIA agent launches a dangerous operation to rescue six Americans in Tehran during the U.S. hostage crisis in Iran in 1979.",
+         rating: 95,
+         image: "https://resizing.flixster.com/Ac5mfOAapSXRP4snhS6aG4ynDrg=/206x305/v2/https://flxt.tmsimg.com/assets/p9128357_p_v10_an.jpg",
+      },
+      {
+         name : "Arrival",
+         summary: "A linguist works with the military to communicate with alien lifeforms after twelve mysterious spacecrafts appear around the world.",
+         rating: 93,
+         image: "https://resizing.flixster.com/OvdPc2UmVxUlKqimb9jA-wrFb1s=/206x305/v2/https://resizing.flixster.com/cR4Yoq8QlGb966w0Ru4eMaGaLw4=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzBlNjgwNTY3LTgzZGUtNDBkYi05MmRjLTVjZTY2ZmM5YWIxMi53ZWJw",
+      },
+   ],
+   horror: [
+      {
+         name : "Us",
+         summary: "A family's serene beach vacation turns to chaos when their doppelgängers appear and begin to terrorize them.",
+         rating: 91,
+         image: "https://resizing.flixster.com/VHGL7Yfk-O1bz9OLwoCjp9Yl5bo=/206x305/v2/https://resizing.flixster.com/S2tuVOVa1x4-Myimy6g1kItVlmM=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzL2MyZWMxMmM0LTdmOTMtNDU3OC05MThiLTk2OTU5YjFkZDhjZi53ZWJw",
+      },
+      {
+         name : "A Quiet Place",
+         summary : "In a post-apocalyptic world, a family is forced to live in silence while hiding from monsters with ultra-sensitive hearing.",
+         rating: 88,
+         image: "https://resizing.flixster.com/nMjwKNgmZ7LFKavtO9LP7LXK290=/206x305/v2/https://resizing.flixster.com/jUkQ7SgQdWoI3xnz1OdwSxEaI2c=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzNmYjhmOGMzLWU1YjctNGEyNS04OTJhLWY4YmIwMjNmZmFiMC53ZWJw",
+      },
+      {
+         name : "Midsommar",
+         summary : "A couple travels to Eastern Europe to visit a rural hometown's fabled Swedish mid-summer festival. What begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.",
+         rating: 85,
+         image: "https://resizing.flixster.com/JhybTWvkR90M1-KPQ3TklSqa94Q=/206x305/v2/https://resizing.flixster.com/3Q-le8L5EBRUHHEG28Uz2dfF9FA=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzL2Q0YmFmZWRhLTY0MjYtNGI5OS1hZGQyLTUwNmI1ZTZlNjcxNC53ZWJw",
+      },
+   ]
+
+}
+
